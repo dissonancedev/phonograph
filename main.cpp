99abf97cce9b69ba8487f3aa9d80153649b8ceef
nikos <nparasta@gmail.com>
2014-04-15 15:56:16 +0200

    Window icon wasn't working. Now it is :-)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon( QIcon(":/images/background/theme/bouzouki.ico") );
     Phonograph w;
+    w.setWindowIcon( QIcon(":/images/background/theme/bouzouki.ico") );
     w.show();
 
     return a.exec();
